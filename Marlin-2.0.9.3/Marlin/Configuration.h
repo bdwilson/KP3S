ad6b90809f27ca286444ce9bd381eaac872c7129
Brian Wilson <wilson@bubba.org>
2022-04-04 14:34:44 -0400

    updates

--- Marlin-2.0.9.3/Marlin/Configuration.h
+++ Marlin-2.0.9.3/Marlin/Configuration.h
@@ -895,7 +895,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 910 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 848 }
 
 /**
  * Default Max Feed Rate (mm/s)
